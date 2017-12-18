729a6e9f60634de9a37fafd25acfed365c3bddc3
Alex Gentile <agentilela@gmail.com>
2017-12-18 08:17:20 -0800

    more cleanup

--- process.py
+++ process.py
@@ -1,55 +1,94 @@
-import os
+"""Batch Update FB Post/Photo Privacy."""
+import calendar
 import json
 import string
-import calendar
-import time
 from datetime import date
-from httplib import *
-from os.path import join, dirname
+from httplib import HTTPSConnection
+from os import environ
+from os.path import dirname, join
 from dotenv import load_dotenv
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+load_dotenv(join(dirname(__file__), '.env'))
 
-SINCE=calendar.timegm(date(2011,1,2).timetuple())
-UNTIL=calendar.timegm(date(2018,1,1).timetuple())
-INTERVAL=86400
-privacy_setting='286958161406148' # only me
+# Config
+PRIVACY_SETTING = '286958161406148'  # only me
+TOKEN = environ.get("TOKEN")
+USER_ID = environ.get("USER_ID")
+BODY = environ.get("BODY")
+COOKIE = environ.get("COOKIE")
+SINCE = calendar.timegm(date(2008, 1, 2).timetuple())
+UNTIL = calendar.timegm(date(2018, 1, 1).timetuple())
 
-TOKEN=os.environ.get("TOKEN")
-USER_ID=os.environ.get("USER_ID")
-BODY=os.environ.get("BODY")
-COOKIE=os.environ.get("COOKIE")
+# HTTP Settings
+UPDATE_PATH = '/privacy/selector/update/'
+URL_PARAMS_1 = "is_saved_on_select=true&should_return_tooltip=false"
+URL_PARAMS_2 = "prefix_tooltip_with_app_privacy=false&replace_on_select=false&dpr=1"
+LOCATION = 'stream'
+CONTENT_TYPE = "application/x-www-form-urlencoded"
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"
+HEADER = {
+    "cookie": COOKIE,
+    "Content-type": CONTENT_TYPE,
+    "user-agent": USER_AGENT
+}
 
-for cur_since in range(SINCE, UNTIL, INTERVAL):
-    REQ_STRING='/me?access_token={TOKEN}&fields=posts.until({UNTIL}).since({SINCE})'.format(TOKEN=TOKEN,SINCE=cur_since, UNTIL=cur_since+INTERVAL,)
-    print "Requesting {REQ_STRING}".format(REQ_STRING=REQ_STRING)
+
+def set_privacy(post_id):
+    """Set privacy of a single post."""
+    raw_update_url = "{a}?privacy_fbid={b}&post_param={c}&render_location_enum={d}&{e}&ent_id={b}"
+    update_url = raw_update_url.format(
+        a=UPDATE_PATH, b=post_id, c=PRIVACY_SETTING, d=LOCATION, e=URL_PARAMS_1 + URL_PARAMS_2, )
+    print "Calling URL {URL}".format(URL=update_url)
+    conn2 = HTTPSConnection("www.facebook.com")
+    conn2.request("POST", update_url, BODY, HEADER)
+    res2 = conn2.getresponse()
+    raw_output = "Setting {post_id} result code: {STATUS}"
+    print raw_output.format(post_id=post_id, STATUS=res2.status)
+
+
+def update_posts(posts):
+    """Loop through posts."""
+    if not 'data' in posts:
+        raw_error = "No postids found {posts}"
+        print raw_error.format(posts=json.dumps(posts, indent=4, sort_keys=True))
+    else:
+        print "Updating {len} posts ".format(len=len(posts['data']))
+        for parsed_post in posts['data']:
+            post_id = string.split(parsed_post['id'], '_')[1]
+            set_privacy(post_id)
+        if 'paging' in posts and 'next' in posts['paging']:
+            next_url = posts['paging']['next']
+            print "got next url {next_url}".format(next_url=next_url)
+            next_responses = collect_posts(next_url)
+            update_posts(next_responses)
+
+
+def collect_posts(collect_url):
+    """Get posts from url."""
     conn = HTTPSConnection("graph.facebook.com")
-    conn.request("GET", REQ_STRING)
+    conn.request("GET", collect_url)
 
     res = conn.getresponse()
     if res.status != 200:
-        print "Get postids failed"
-    #print res.read()
-    d = json.load(res)
-
-    if not 'posts' in d:
-        print "No postids found between {SINCE} and {UNTIL}".format(SINCE=cur_since,UNTIL=cur_since+INTERVAL)
+        print "Get postids failed {STATUS}".format(STATUS=res.status)
     else:
-        for dd in d['posts']['data']:
-            post_id = string.split(dd['id'], '_')[1]
-            body = BODY
-            header = {
-                    "cookie": COOKIE,
-                    "Content-type": "application/x-www-form-urlencoded",
-                    "user-agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1"
-                    }
-            conn2 = HTTPSConnection("www.facebook.com")
-            conn2.request("POST", "/privacy/selector/update/?privacy_fbid="+post_id+
-                "&post_param="+privacy_setting+
-                "&render_location_enum=stream&is_saved_on_select=true&should_return_tooltip=false&prefix_tooltip_with_app_privacy=false&replace_on_select=false&dpr=1&ent_id="+post_id, body, header)
+        return json.load(res)
 
-            res2 = conn2.getresponse()
-        print "Setting "+post_id+" result code="+str(res2.status)
-        if res2.status != 200 :
-            print res2.read()
+
+def main():
+    """Main Loop."""
+    raw_collect_url = '/me?access_token={TOKEN}&fields=photos,posts.until({UNTIL}).since({SINCE})'
+    collect_url = raw_collect_url.format(TOKEN=TOKEN, SINCE=SINCE, UNTIL=UNTIL)
+    print "Requesting {collect_url}".format(collect_url=collect_url)
+
+    parsed_responses = collect_posts(collect_url)
+    if parsed_responses and 'posts' in parsed_responses:
+        update_posts(parsed_responses['posts'])
+    else:
+        raw_error = "No postids found {parsed_responses} at ${collect_url}"
+        pretty_responses = json.dumps(
+            parsed_responses, indent=4, sort_keys=True)
+        print raw_error.format(parsed_responses=pretty_responses, collect_url=collect_url)
+
+
+main()
